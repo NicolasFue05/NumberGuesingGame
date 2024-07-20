87654ea2260ec291d81361210db0c4d413d397cb
Nicolas Fuentes <166555512+NicolasFue05@users.noreply.github.com>
2024-07-20 13:23:20 -0500

    :feat: const added

---
+++ script.js
@@ -0,0 +1,13 @@
+// Number guessing game
+
+//scoreboard
+const tries = document.getElementById('tries');
+const score = document.getElementById('score');
+
+// Words inputs and display message
+const wordInput = document.getElementById('wordInput');
+const wordMessage = document.getElementById('messageError');
+const wordDisplay = document.getElementById('word');
+
+// try Again/play again button
+const button = document.getElementById('buttongame')
